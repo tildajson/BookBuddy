a813a653ec0a245abfcf1393f6210f524f8bb38e
Tilda Johansson <130234732+tildajson@users.noreply.github.com>
2024-01-16 15:58:58 +0100

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['web-production-7256.up.railway.app']
 
